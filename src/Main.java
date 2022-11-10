fa61a8640ea1daa8721eb2a3222d6f02ef5a0116
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-11-10 21:58:36 +0300

    Task 1 Done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int [] simpleNumbers = new int [] {1, 2, 3};
-        double [] numbers = new double [] {1.57, 7.654 ,9.986};
-        float [] random = new float [] {2.555f, 28.784f, 34.552f, 109.8567f};
+        int[] simpleNumbers = new int[]{1, 2, 3};
+        simpleNumbers[0] = 1;
+        simpleNumbers[1] = 2;
+        simpleNumbers[2] = 3;
+        for (int i = 0; i < simpleNumbers.length; i++) {
+            System.out.println(simpleNumbers[i]);
+        }
+        double[] numbers = new double[]{1.57, 7.654, 9.986};
+        float[] random = new float[]{2.555f, 28.784f, 34.552f, 109.8567f};
     }
 }
